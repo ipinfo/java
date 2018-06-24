a32518b2e0dcc04a0722fb1908bc5af9b142c9e9
Amir Omidi <amir@aaomidi.com>
2018-06-24 01:47:28 -0700

    Add initial class.

---
+++ src/main/java/io/ipinfo/IPInfo.java
@@ -0,0 +1,10 @@
+package io.ipinfo;
+
+public class IPInfo {
+
+    public static void main(String... args) {
+        System.out.println("This library is not meant to be run as a standalone jar.");
+
+        System.exit(0);
+    }
+}
