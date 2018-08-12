790e24e74e4f433b19bf2757b367f6fd0ca49669
Amir Omidi <amir@aaomidi.com>
2018-08-11 17:53:55 -0700

    Start on the build pipeline.

---
+++ util/deploy_docs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e -u
+
+if [ "$TRAVIS_REPO_SLUG" == "google/guava" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Publishing Javadoc and JDiff..."
+
+  cd $HOME
+  git clone -q -b gh-pages https://${GH_TOKEN}@github.com/ipinfo/java-ipinfo gh-pages > /dev/null
+  cd gh-pages
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+
+  ./updaterelease.sh snapshot
+
+  git push -fq origin gh-pages > /dev/null
+
+  echo "Javadoc and JDiff published to gh-pages."
+fi
