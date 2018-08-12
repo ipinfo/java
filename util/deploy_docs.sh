da4d7cad1555f114a8a6a96df8dce7861263c2de
Amir Omidi <amir@aaomidi.com>
2018-08-11 18:09:25 -0700

    Update document script

--- util/deploy_docs.sh
+++ util/deploy_docs.sh
@@ -2,20 +2,25 @@
 
 set -e -u
 
+
 if [ "$TRAVIS_REPO_SLUG" == "google/guava" ] && \
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Publishing Javadoc and JDiff..."
-
-  cd $HOME
-  git clone -q -b gh-pages https://${GH_TOKEN}@github.com/ipinfo/java-ipinfo gh-pages > /dev/null
-  cd gh-pages
-
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
 
-  ./updaterelease.sh snapshot
+  cd java-ipinfo/target
+
+  git clone -q -b gh-pages https://${GH_TOKEN}@github.com/ipinfo/java-ipinfo gh-pages > /dev/null
+  cd gh-pages
+
+  cp -rf ../apidocs/* .
+
+  git add --all
+
+  git commit -m "Deploy javadocs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 
   git push -fq origin gh-pages > /dev/null
 
