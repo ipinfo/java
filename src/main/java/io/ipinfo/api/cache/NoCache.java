74fd4d062cbc231870e6979e5fe8c48aecc9759d
Amir Omidi <amir@aaomidi.com>
2018-09-05 09:01:36 +0430

    Add no cache as an option

---
+++ src/main/java/io/ipinfo/api/cache/NoCache.java
@@ -0,0 +1,32 @@
+package io.ipinfo.api.cache;
+
+
+import io.ipinfo.api.model.ASNResponse;
+import io.ipinfo.api.model.IPResponse;
+
+public class NoCache implements Cache {
+    @Override
+    public IPResponse getIp(String ip) {
+        return null;
+    }
+
+    @Override
+    public ASNResponse getAsn(String asn) {
+        return null;
+    }
+
+    @Override
+    public boolean setIp(String ip, IPResponse response) {
+        return false;
+    }
+
+    @Override
+    public boolean setAsn(String asn, ASNResponse response) {
+        return false;
+    }
+
+    @Override
+    public boolean clear() {
+        return false;
+    }
+}
