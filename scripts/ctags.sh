13dd4a849d6a241e4f7b196ba5709b482cad103d
Uman Shahzad <uman@ipinfo.io>
2021-08-01 19:07:16 +0500

    random prettification

--- scripts/ctags.sh
+++ scripts/ctags.sh
@@ -2,13 +2,13 @@
 
 # Regenerate ctags.
 
-ctags \
-    --recurse=yes \
-    --exclude=node_modules \
-    --exclude=dist \
-    --exclude=build \
-    --exclude=target \
-    -f .vim/tags \
-    --tag-relative=never \
-    --totals=yes \
+ctags                                                                         \
+    --recurse=yes                                                             \
+    --exclude=node_modules                                                    \
+    --exclude=dist                                                            \
+    --exclude=build                                                           \
+    --exclude=target                                                          \
+    -f .vim/tags                                                              \
+    --tag-relative=never                                                      \
+    --totals=yes                                                              \
         ./src
