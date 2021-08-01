be727b157174186cdb227b3a002236c6b59d1e9a
Uman Shahzad <uman@ipinfo.io>
2021-08-01 15:52:10 +0500

    fix deploy docs script.

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -5,12 +5,11 @@ set -eu
 echo "Publishing Javadoc and JDiff..."
 
 cd ./target
-git clone -b gh-pages https://github.com/ipinfo/java gh-pages > /dev/null
+git clone -b gh-pages git@github.com:ipinfo/java gh-pages
 cd gh-pages
 cp -rf ../apidocs/* .
-
 git add --all
-git commit -m "Deploy javadocs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-git push -fq origin gh-pages > /dev/null
+git commit -m "Deploy javadocs to GitHub Pages."
+git push -f origin gh-pages
 
 echo "Javadoc and JDiff published to gh-pages."
